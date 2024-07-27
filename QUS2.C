6163aa9fec6611cc6e36b553fc5762fcb317a411
dmetrix25 <rmilind06@gmail.com>
2024-07-27 10:34:15 +0530

    Create QUS2.C

---
+++ QUS2.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_LENGTH 100
+void reverseString(char str[]) {
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+int main() {
+    char str[MAX_LENGTH];
+    printf("Enter a string: ");
+    fgets(str, MAX_LENGTH, stdin);
+    str[strcspn(str, "\n")] = '\0';
+    reverseString(str);
+    printf("Reversed string: %s\n", str);
+    return 0;
+}
